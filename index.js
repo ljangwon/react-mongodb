6b2eceae935fc7ceedf93c981086ab557c8f9511
JakeLee <jakeleanco@gmail.com>
2024-02-28 23:30:11 +0900

    login/logout

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const port = '3001';
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const { User } = require('./models/User');
+const { auth } = require('./middleware/auth');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -29,17 +30,15 @@ app.get('/', function (req, res) {
 	res.send('Hello World! 새해 복많이 받으세요');
 });
 
-app.post('/register', (req, res) => {
+app.post('/api/users/register', (req, res) => {
 	const user = new User(req.body);
 	user.save((err, userInfo) => {
-		if (err)
-			//
-			return res.json({ sucess: false, err });
+		if (err) return res.json({ sucess: false, err });
 		return res.status(200).json({ sucess: true, userInfo });
 	});
 });
 
-app.post('/login', (req, res) => {
+app.post('/api/users/login', (req, res) => {
 	//요청된 이메일을 데이터베이스에서 찾는다.
 	//요청된 이메일이 있으면 비밀번호가 맞는지 확인
 	//비밀번호가 맞다면 토큰을 생성하기
@@ -52,14 +51,46 @@ app.post('/login', (req, res) => {
 		}
 
 		user.comparePassword(req.body.password, (err, isMatch) => {
-			if (!isMatch) return res.json({ loginSuccess: false, message: '비밀번호가 틀렸습니다.' });
+			if (!isMatch)
+				return res.json(
+					//
+					{ loginSuccess: false, message: '비밀번호가 틀렸습니다.' }
+				);
 			user.generateToken((err, user) => {
 				if (err) return res.status(400).send(err);
 				// 토큰을 저장한다. 어디에? 쿠키, session, 로컬스토리지
-				res.cookie('x_auth', user.token).status(200).json({ loginSuccess: true, userId: user._id });
+				res.cookie('x_auth', user.token).status(200).json(
+					//
+					{
+						loginSuccess: true, //
+						user: user,
+					}
+				);
 			});
 		});
 	});
 });
 
+app.get('/api/users/auth', auth, (req, res) => {
+	//여기까지 미들웨어를 통과해 왔다는 것은 Authentication 이 True 라는 말
+	res.status(200).json({
+		_id: req.user._id,
+		isAdmin: req.user.role === 0 ? false : true,
+		email: req.user.email,
+		name: req.user.name,
+		lastname: req.user.lastname,
+		role: req.user.role,
+		image: req.user.image,
+	});
+});
+
+app.get('/api/users/logout', auth, (req, res) => {
+	User.findOneAndUpdate({ _id: req.user._id }, { token: '' }, (err, user) => {
+		if (err) return res.json({ sucess: false, err });
+		return res.status(200).json({
+			sucess: true,
+			user: user,
+		});
+	});
+});
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- models/User.js
+++ models/User.js
@@ -66,6 +66,19 @@ userSchema.methods.generateToken = function (cb) {
 	});
 };
 
+userSchema.statics.findByToken = function (token, cb) {
+	let user = this;
+	// 토큰을 복호화 한다.
+	jwt.verify(token, 'secretToken', function (err, decoded) {
+		//유저 아이디를 이용해서 유저를 찾은 다음에
+		//클라이언트에서 가져온 토큰과 디비에 보관된 토튼이 일치하는지 확인
+		user.findOne({ _id: decoded, token: token }, function (err, user) {
+			if (err) return cb(err);
+			cb(null, user);
+		});
+	});
+};
+
 const User = mongoose.model('User', userSchema);
 
 module.exports = { User };
